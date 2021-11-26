6ac851049e84b9c5def0d05ad6f37ec15d80bd20
kevinchensunshine <kevinlc3@illinois.edu>
2021-11-25 20:20:05 -0600

    Patching up missed getPreferences

--- app/src/main/java/edu/illinois/cs/cs124/ay2021/mp/network/Client.java
+++ app/src/main/java/edu/illinois/cs/cs124/ay2021/mp/network/Client.java
@@ -103,7 +103,7 @@ public final class Client {
     requestQueue.add(restaurantsRequest);
   }
   public void getPreferences(final Consumer<List<Preference>> callback) {
-    StringRequest preferencesRequest =
+    StringRequest preferenceRequest =
             new StringRequest(
                     Request.Method.GET,
                     EatableApplication.SERVER_URL + "preferences/",
@@ -132,7 +132,7 @@ public final class Client {
                       callback.accept(null);
                     });
     // Actually queue and run the request
-    requestQueue.add(preferencesRequest);
+    requestQueue.add(preferenceRequest);
   }
   /*
    * You do not need to modify the code below.

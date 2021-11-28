fab37059b346af113564f0d79c898d975a959481
kevinchensunshine <kevinlc3@illinois.edu>
2021-11-27 21:04:48 -0600

    MP2 finished

--- app/src/main/java/edu/illinois/cs/cs124/ay2021/mp/models/Preference.java
+++ app/src/main/java/edu/illinois/cs/cs124/ay2021/mp/models/Preference.java
@@ -1,4 +1,16 @@
 package edu.illinois.cs.cs124.ay2021.mp.models;
+
+
 //MP2 part 2
-public class Preference {
+public final class Preference {
+  private String id;
+  private String[] restaurantIDs;
+  private Preference() {
+  }
+  public String getId() {
+    return id;
+  }
+  public String[] getRestaurantIDs() {
+    return restaurantIDs;
+  }
 }
--- app/src/main/java/edu/illinois/cs/cs124/ay2021/mp/models/RelatedRestaurants.java
+++ app/src/main/java/edu/illinois/cs/cs124/ay2021/mp/models/RelatedRestaurants.java
@@ -2,19 +2,41 @@ package edu.illinois.cs.cs124.ay2021.mp.models;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.HashSet;
+import java.util.Set;
 
 public class RelatedRestaurants {
   private Map<String, Map<String, Integer>> restaurantRelationships = new HashMap<>();
 
   public RelatedRestaurants(final List<Restaurant> restaurants, final List<Preference> preferences) {
+    Set<String> checkedRestaurants = new HashSet();
+    for (Restaurant r : restaurants) {
+      checkedRestaurants.add(r.getId());
+    }
     for (Restaurant r: restaurants) {
-      for (Preference p: preferences) {
-        restaurantRelationships.put("String", getRelated(r.getId()));
+      Map<String, Integer> related = new HashMap<>();
+      for (Preference p : preferences) {
+        for (String g : p.getRestaurantIDs()) {
+          if (r.getId().equals(g)) {
+            for (String m : p.getRestaurantIDs()) {
+              if (related.containsKey(m) && m != r.getId() && checkedRestaurants.contains(m)) {
+                related.put(m, related.get(m) + 1);
+              } else if (m != r.getId() && checkedRestaurants.contains(m)) {
+                related.put(m, 1);
+              }
+            }
+          }
+        }
       }
+      restaurantRelationships.put(r.getId(), related);
     }
   }
-
   public Map<String, Integer> getRelated(final String restaurantID) {
-    return null;
+    Map<String, Integer> returnMap = restaurantRelationships.get(restaurantID);
+    if (returnMap == null) {
+      return new HashMap<>();
+    } else {
+      return returnMap;
+    }
   }
 }
